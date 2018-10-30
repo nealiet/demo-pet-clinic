3911daa3f8b1f1e108e9d857e42a98e98d94ac5f
nealiet <nealiet@comcast.net>
2018-10-30 14:17:34 -0400

    Added Id to Pojo Closes #3

---
+++ pet-clinic-data/src/main/java/com/teilaen/demopetclinic/model/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.teilaen.demopetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    
+    // Hibernate recommends 'Box' types
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
