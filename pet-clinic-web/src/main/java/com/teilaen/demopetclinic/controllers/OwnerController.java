aae159f87141efc24047c4daa055e66f1827f3bd
nealiet <nealiet@comcast.net>
2018-10-30 20:17:34 -0400

    Closes #11 Closes #12

--- pet-clinic-web/src/main/java/com/teilaen/demopetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/teilaen/demopetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.teilaen.demopetclinic.controllers;
 
+import com.teilaen.demopetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-        @RequestMapping({"", "/","/index","/index.html"})
-        public String listOwners() {
+    private final OwnerService ownerService;
+
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/","/index","/index.html"})
+        public String listOwners(Model model) {
+            model.addAttribute("owners",ownerService.findAll());
             return "owners/index";
         }
     }
