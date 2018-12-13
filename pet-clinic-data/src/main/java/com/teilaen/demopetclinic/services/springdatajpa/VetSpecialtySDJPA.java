316e8ef42b34c8d1c741afe66f7d0e2f4a09a035
nealiet <nealiet@comcast.net>
2018-12-13 16:52:18 -0500

    Closes #28 Testing

---
+++ pet-clinic-data/src/main/java/com/teilaen/demopetclinic/services/springdatajpa/VetSpecialtySDJPA.java
@@ -0,0 +1,48 @@
+package com.teilaen.demopetclinic.services.springdatajpa;
+
+import com.teilaen.demopetclinic.model.Specialty;
+import com.teilaen.demopetclinic.repositories.SpecialtyRepository;
+import com.teilaen.demopetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSpecialtySDJPA implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public VetSpecialtySDJPA(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+           return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
