7606993c8e1beeb45523203fa8d4d56922ea714a
nealiet <nealiet@comcast.net>
2018-12-13 15:01:57 -0500

    Close #36

--- pet-clinic-data/src/main/java/com/teilaen/demopetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/teilaen/demopetclinic/model/BaseEntity.java
@@ -1,12 +1,16 @@
 package com.teilaen.demopetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-
-
-    // Hibernate recommends 'Box' types
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
